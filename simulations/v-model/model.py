20304237127908295e79adf0b855110a5d7f82b3
Victor Guedes <victorgcramos@gmail.com>
2019-09-16 11:59:31 -0300

    finish entities

--- simulations/v-model/model.py
+++ simulations/v-model/model.py
@@ -20,8 +20,13 @@ class Citizen:
 
   # TODO: Docs for get_opinion
   def get_opinion(self, proposal):
-    return self.interestNormalDist.cdf(proposal)
-    pass
+    prob = self.interestNormalDist.cdf(proposal)
+    approved = np.random.uniform() >= (1 - prob)
+    if approved:
+      self.feedbacks.positive += 1
+    else:
+      self.feedbacks.negative += 1
+    return self
 
   # TODO: Docs for vote
   def vote(self, candidates_list: list):
@@ -29,9 +34,6 @@ class Citizen:
       self.interestNormalDist.overlap(candidate.interestNormalDist)
     , candidates_list)
     return similarity_list.index(max(similarity_list))
-    pass
-
-
 
   def __init__(self):
     self.id = next(Citizen.id_iter)
@@ -47,14 +49,13 @@ class Representative(Citizen):
   # TODO: docs for new_proposal
   def new_proposal(self):
     return self.interestNormalDist.samples(1)
-    pass
-
 
 class State():
   election_history: list
   citizens: list
   candidates: list
   year: int
+  repersentative: Representative
 
   def election(self):
     votes = map(lambda citizen: citizen.vote(self.candidates), self.citizens)
@@ -63,7 +64,7 @@ class State():
       vote_count[vote] += 1
     
     result = vote_count.index(max(vote_count))
-    self.repersentative = self.candidates[result]
+    self.repersentative = Representative(self.candidates[result])
     election_stats = {
       year: self.year,
       votes: vote_count,
@@ -72,7 +73,6 @@ class State():
     }
     self.election_history.append(election_stats)
     return election_stats
-    pass
 
   # returns a list of candidates
   def choice_of_candidates(self):
@@ -83,11 +83,32 @@ class State():
       candidates.append(candidate)
     self.candidates = candidates
     return candidates
-    pass
 
   def day_simulation(self):
+    # 1 - The Representative releases a proposal
+    proposal = self.repersentative.new_proposal()
 
-    pass
+    # 2 - The proposal is evaluated by the citizens
+    map(
+      lambda citizen: citizen.get_opinion(proposal)
+    , self.citizens)
+
+  def get_population_feedback(self):
+    feedbacks: list = []
+    for citizen in self.citizens:
+      feedbacks.append(citizen.feedbacks)
+    return feedbacks
+
+  def mandate(self, time: int):
+    self.choice_of_candidates()
+    election_result = self.election()
+    for i in range(time):
+      self.day_simulation()
+    pfb = self.get_population_feedback()
+    return {
+      election: election_result,
+      population_feedback: pfb
+    }
 
   def __init__(self, population_number, candidates_number):
     self.year = 0
@@ -96,7 +117,11 @@ class State():
     self.candidates = []
     self.candidates_number = candidates_number
     self.repersentative = None
-  pass
 
-
-print(x)
+if __name__ == "__main__":
+  state = State(200, 5);
+  mandate_time = 1460 # 4 years
+  simulation_time = 100000
+  for i in range(simulation_time):
+    active_mandate_time = 0
+    for i in range(active_mandate_time)
